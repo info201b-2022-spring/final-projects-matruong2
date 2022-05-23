18263678a330d8fec4df18c55e602a1dd3f36df5
JiratR <102629749+JiratR@users.noreply.github.com>
2022-05-22 21:28:20 -0700

    fixed accidental change
    deleted something i accidentally added from my rstudio

--- State Rate Chart.R
+++ State Rate Chart.R
@@ -55,5 +55,3 @@ p <- ggplot(data = US_data_grp_state, aes(x=as.factor(US_data_grp_state$STATE), 
   # Add the labels, using the label_data dataframe that we have created before
   geom_text(data=US_data_grp_state, aes(x=US_data_grp_state$STATE, y=US_data_grp_state$RATE, label=individual, hjust=hjust), color="black", fontface="bold",alpha=0.6, size=2.5, angle= label_data$angle, inherit.aes = FALSE ) 
 coord_polar(start = 0)
-
-print(p)
