1d3e36e343e8889d1c9c7cb90f4a1c82ac2a4187
tylertakeuchi <tylertak@uw.edu>
2022-05-22 22:13:30 -0700

    New Plot

--- State Rate Chart.R
+++ State Rate Chart.R
@@ -32,26 +32,14 @@ label_data$angle<-ifelse(angle < -90, angle+180, angle)
 
 
 # Start the plot
-p <- ggplot(data = US_data_grp_state, aes(x=as.factor(US_data_grp_state$STATE), y=US_data_grp_state$RATE)) +       # Note that id is a factor. If x is numeric, there is some space between the first bar
+p <- ggplot(data = US_data_grp_state, aes(x=as.factor(STATE), y=RATE)) +       # Note that id is a factor. If x is numeric, there is some space between the first bar
   
   # This add the bars with a blue color
   geom_bar(stat="identity", fill=alpha("skyblue", 0.7)) +
-  
-  # Limits of the plot = very important. The negative value controls the size of the inner circle, the positive one is useful to add size over each bar
-  ylim(-100,120) +
-  
-  # Custom the theme: no axis title and no cartesian grid
-  theme_minimal() +
-  theme(
-    axis.text = element_blank(),
-    axis.title = element_blank(),
-    panel.grid = element_blank(),
-    plot.margin = unit(rep(-1,4), "cm")      # Adjust the margin to make in sort labels are not truncated!
-  ) +
   
   # This makes the coordinate polar instead of cartesian.
   coord_polar(start = 0) +
   
   # Add the labels, using the label_data dataframe that we have created before
-  geom_text(data=US_data_grp_state, aes(x=US_data_grp_state$STATE, y=US_data_grp_state$RATE, label=US_data_grp_state$STATE, hjust=hjust), color="black", fontface="bold",alpha=0.6, size=2.5, angle= label_data$angle, inherit.aes = FALSE ) 
+  geom_text(data=US_data_grp_state, aes(x=STATE, y=RATE, label=STATE, hjust=hjust), color="black", fontface="bold",alpha=0.6, size=2.5, angle= label_data$angle, inherit.aes = FALSE ) 
 coord_polar(start = 0)
