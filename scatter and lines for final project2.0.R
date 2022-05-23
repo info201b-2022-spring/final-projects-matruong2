c9ab7300f342fec1d3886591bd4bbcde504965d4
etallman1 <102687693+etallman1@users.noreply.github.com>
2022-05-22 20:21:16 -0700

    Add files via upload

---
+++ scatter and lines for final project2.0.R
@@ -0,0 +1,41 @@
+library(readr)
+library(ggplot2)
+library(dplyr)
+library(tidyverse)
+library(stringr)
+library(hrbrthemes)
+
+#Data Cleaning
+<<<<<<< HEAD
+WorldSuicide <- read_csv("who_suicide_statistics.csv")
+=======
+WorldSuicide <- read_csv("data/who_suicide_statistics.csv")
+>>>>>>> dca59663891c0a4aa8df47c877a0a3fe3f86d855
+
+suicidebyyearUSA <- filter(
+  WorldSuicide,
+  country == "United States of America") %>%
+  filter(year >= 2000)%>% 
+  group_by(year) %>% 
+  summarize(suicides_no = sum(suicides_no))%>%
+  na.omit(WorldSuicide)
+
+#to check the "cleaned up"
+print(suicidebyyearUSA)
+
+#Variables:
+#suicidebyyearUSA(Used to make line/scatter)
+#WorldSuicide(Raw data)
+
+#R-graph-gallery used to create graph of scatter+line
+
+ggplot(suicidebyyearUSA, aes(x = year, y = suicides_no)) +
+  geom_point() +
+  labs(x="Years", y="No.of Suicides", title = "jirat smells") +
+  geom_line(color="gray28") +
+<<<<<<< HEAD
+  geom_point(shape=23, color="#000000", fill="#261bcc", size=4) 
+  
+=======
+  geom_point(shape=23, color="#000000", fill="#261bcc", size=4)
+>>>>>>> dca59663891c0a4aa8df47c877a0a3fe3f86d855
