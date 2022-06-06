ba5d72b0350f22c433cad3caeb8140ef4ef23e42
matruong2 <matruong@uw.edu>
2022-06-05 19:58:18 -0700

    started on working on interactive map visual

--- app.R
+++ app.R
@@ -1,7 +1,12 @@
-
-
+library(tidyverse)
 library(shiny)
 library(ggplot2)
+library(usmap)
+
+US_data <- read.csv("data/suicide_mortality.csv")
+colnames(US_data)[2] <- "state"
+US_data <- US_data %>% 
+  filter( YEAR != 2005)
 
 intro_page <- 
   tabPanel(
@@ -33,10 +38,25 @@ second_tab <-
 
 third_tab <- 
   tabPanel(
-    "Tab Three",
+    "Map Visualization",
     fluidPage(
-      h1("Whatever is gonna go here"),
-      p("more interactive stuff")
+      titlePanel("U.S. Map of Suicide Death Rates"),
+      p("Maybe some kind of intro describing data shown"),
+      sidebarLayout(
+        sidebarPanel(
+          sliderInput(
+            inputId = "mapYear",
+            label = "Year",
+            min = min(US_data$YEAR),
+            max = max(US_data$YEAR),
+            value = min(US_data$YEAR), #I might change to double slider
+            step = 1
+          )
+        ),
+        mainPanel(
+          plotOutput(outputId = "map")
+        )
+      )
     )
   )
 
@@ -71,7 +91,25 @@ ui <- (                         ## call this website my website
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    
+  
+  # server code for the map visualization
+  make_map_df <- function(year) { 
+    map_df <- US_data %>% 
+      filter(YEAR == year) #%>% 
+      #doesn't really do much since each year has only one piece of data
+      # could change so that avg_rate can be calculated from different year ranges
+      #group_by(state) %>% 
+      #summarize(avg_rate = mean(RATE, na.rm = TRUE)) 
+    return(map_df)
+  }
+  
+  output$map <- renderPlot({
+    plot_title <- str_c("Death Rates by State in ", input$mapYear)
+    plot_usmap(data = make_map_df(input$mapYear), values = "RATE") +
+      labs(title = plot_title ) +
+      theme(plot.title = element_text(size=22)) +
+      scale_fill_continuous(low = "white", high = "purple3")
+  })
 }
 
 # Run the application 
